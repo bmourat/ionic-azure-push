d6e887b0fa3bb12d6a09710ee1c91c275f97fb88
Murat Baysangurov <v-mubays@microsoft.com>
2018-10-24 11:59:29 +0300

    Added pushes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
+declare var WindowsAzure: any;
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,14 +12,48 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  client:any;
+  azurePush:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public push: Push,) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.client = new WindowsAzure.MobileServiceClient("https://bmourat.azurewebsites.net");
+      this.initPushNotification();
     });
+  }
+
+  initPushNotification() {
+    if (!this.platform.is('cordova')) {
+      console.warn('Push notifications not initialized. Cordova is not available - Run in physical device');
+      return;
+    }
+    const options: PushOptions = {
+      android: {
+        senderID: '1031320738669'
+      },
+      ios: {
+        alert: 'true',
+        badge: false,
+        sound: 'true'
+      },
+      windows: {}
+    };
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('registration').subscribe((data: any) => {
+      console.log('device token -> ' + data.registrationId);
+      //this.client.
+    });
+
+    pushObject.on('notification').subscribe((data: any) => {
+      console.log('message -> ' + data.message);
+    });
+
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin' + error));
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { PushObject, PushOptions, Push } from '@ionic-native/push';
+
+declare var WindowsAzure: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  client: any;
+  constructor(platform: Platform, public navCtrl: NavController, public push: Push) {
+    platform.ready().then(() => {
+      this.initPushNotification();
+    });
+  }
 
-  constructor(public navCtrl: NavController) {
+  initPushNotification() {
+    this.client = new WindowsAzure.MobileServiceClient('http://bmourat.azurewebsites.net');
+    const options: PushOptions = {
+      android: {
+        senderID: '1031320738669'
+      },
+      ios: {
+        alert: 'true',
+        badge: false,
+        sound: 'true'
+      },
+      windows: {}
+    };
+    const pushObject: PushObject = this.push.init(options);
 
+    pushObject.on('registration').subscribe((data: any) => {
+      console.log('device token -> ' + data.registrationId);
+      //TODO - send device token to server
+      this.client.push.register('gcm', data.registrationId, {
+        mytemplate: { body: { data: { message: "{$(messageParam)}" } } }
+      }, null, function(error, response) {
+        if (error) {
+          console.error('Error registering for pushes: ' + error);
+        } else {
+          console.log('Registering for pushes response: ' + response.statusText);
+        }
+      });
+    });
+
+    pushObject.on('notification').subscribe((data: any) => {
+      console.log('message -> ' + data.message);
+    });
+
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin' + error));
   }
 
 }
